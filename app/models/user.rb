7a54d30e793f25c0f15e8d0cf27d65c43e150b8d
Roman Weißflog <roman.weissflog@gmail.com>
2016-09-11 13:42:45 +0200

    removed debug messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,11 @@ class User < ApplicationRecord
 
   def find_in_db
     other = User.find_by(email: self.email)
-    p "hi"
     return false if other == nil
-    p "huhu"
-    p other.password
-    p self.password
     return other.password == self.password
   end
 
   def get_email
-    p "TESTST"
-    p self.email
     self.email
   end
 
